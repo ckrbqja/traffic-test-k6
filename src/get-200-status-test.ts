25d5f90185becb27dda740a55f6c492dee67f821
chakyubeom <ckrbqja@gmail.com>
2023-02-12 08:24:50 +0900

    first commit

--- src/get-200-status-test.ts
+++ src/get-200-status-test.ts
@@ -1,16 +1,36 @@
-import { sleep, check } from 'k6';
-import { Options } from 'k6/options';
-import http from 'k6/http';
+import {check, group, sleep} from 'k6';
+import {Options} from 'k6/options';
+import http from "k6/http";
+import {Trend} from "k6/metrics";
 
-export let options:Options = {
-  vus: 50,
-  duration: '10s'
+export const TrendRTT = new Trend('RTT');
+export let options: Options = {
+    vus: 1,
+    duration: '10s',
+    thresholds: {
+        'group_duration{group:::individualRequests}': [{abortOnFail: true, threshold: 'avg < 400'}]
+        'TrendRTT': []
+    }
 };
+
 
 export default () => {
-  const res = http.get('https://test-api.k6.io');
-  check(res, {
-    'status is 200': () => res.status === 200,
-  });
-  sleep(1);
+    TrendRTT.add()
+    group('individualRequests', () => {
+        http.get('http://localhost:8081')
+    })
+
+    // const res = http.get('http://test.k6.io');
+    //
+    // check(res, {
+    //     'is status 200': (r) => r.status === 200,
+    //     'body length chk': (r) => r.body?.length === 11278
+    // })
+
+
+    // check(res, {
+    //   'status is 200': () => res.status === 200,
+    // });
+    sleep(1);
 };
+
